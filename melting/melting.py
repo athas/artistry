1faaafdcf1b57051d6e5a36669ba47271c1de841
Troels Henriksen <athas@sigkill.dk>
2016-02-27 15:03:05 +0100

    I did things.

--- melting/melting.py
+++ melting/melting.py
@@ -16,16 +16,22 @@ def write_image(d, w, h, i, pixels):
 def main():
     reader = png.Reader(filename=sys.argv[1])
     (w,h,pixels,meta) = reader.asRGB8()
-    pixels = numpy.reshape(numpy.array(list(pixels)), (h, w, 3))
+    pixels = numpy.reshape(numpy.array(list(pixels), dtype=numpy.byte), (h, w, 3))
 
     def write_frame(pixels):
         global i
         write_image('out', w, h, i, pixels)
         i = i + 1
 
-    frames = melting_frames.main(10, pixels.astype(numpy.byte, copy=True))
-    for frame in frames:
-        write_frame(frame)
+    frames_remaining = 1000
+    chunksize = 100
+    while frames_remaining > 0:
+        render = min(frames_remaining, chunksize)
+        frames = melting_frames.main(render, pixels)
+        for frame in frames:
+            write_frame(frame)
+        frames_remaining = max(0, frames_remaining - chunksize)
+        pixels=frames[render-1].astype(dtype=numpy.byte, copy=True)
 
 if __name__ == '__main__':
     main()
